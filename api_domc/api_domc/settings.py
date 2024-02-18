e792a43d4bfaee8dc5848e466cebf338cfac2d7c
vincentvega614 <vincentvega-94@yandex.ru>
2024-02-18 22:26:27 +0300

    в requirements.txt добавлен gunicorn, в settings.py добавлены настройки для статики для работы nginx

--- api_domc/api_domc/settings.py
+++ api_domc/api_domc/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from datetime import timedelta
 
@@ -123,7 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+STATIC_URL = '/static/' # префикс для url
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/') # папка, в которой будет лежать статика
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
