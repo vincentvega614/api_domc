e6a1fc2b73b2817105ae4dfce59a4a8f915c2419
vincentvega614 <vincentvega-94@yandex.ru>
2025-01-09 00:11:38 +0300

    переработан сериализатор ApartmentBuildingSerializer, теперь поддерживается вывод строкового представления связанных моделей и поддерживается запись новых обектоа (ссылка на связанные модели осуществляется через pk

--- api_domc/api/serializers.py
+++ api_domc/api/serializers.py
@@ -44,20 +44,44 @@ class ApartmentBuildingSerializer(serializers.ModelSerializer):
     # management_company = serializers.StringRelatedField(
     #     read_only=True
     # )
+    management_company = serializers.SerializerMethodField()
     # management_company_site = serializers.StringRelatedField(
     #     read_only=True
     # )
+    management_company_site = serializers.SerializerMethodField()
     # note = serializers.StringRelatedField(read_only=True)
+    management_company_id = serializers.PrimaryKeyRelatedField(
+        queryset=ManagementCompany.objects.all(), write_only=True
+    )
+    management_company_site_id = serializers.PrimaryKeyRelatedField(
+        queryset=ManagementCompanySite.objects.all(), write_only=True
+    )
 
     class Meta:
         model = ApartmentBuilding
         fields = (
             'id', 'building_adress', 'management_company',
-            'management_company_site',
-            'navigation_link_to_the_building', 'in_contract',
-            'pipe_support_aria', 'pipe_support_oyster', 'pipe_support_comlink',
-            'wall_mount_aria', 'wall_mount_oyster', 'wall_mount_comlink'
+            'management_company_site', 'management_company_id',
+            'management_company_site_id', 'navigation_link_to_the_building',
+            'in_contract', 'pipe_support_aria', 'pipe_support_oyster',
+            'pipe_support_comlink', 'wall_mount_aria', 'wall_mount_oyster',
+            'wall_mount_comlink'
         )
+
+    def get_management_company(self, obj):
+        return str(getattr(obj, 'management_company', None))
+
+    def get_management_company_site(self, obj):
+        return str(getattr(obj, 'management_company_site', None))
+
+    def create(self, validated_data):
+        validated_data['management_company'] = validated_data.pop(
+            'management_company_id', None
+        )
+        validated_data['management_company_site'] = validated_data.pop(
+            'management_company_site_id', None
+        )
+        return super().create(validated_data)
 
 
 class ManagementCompanySerializer(serializers.ModelSerializer):
